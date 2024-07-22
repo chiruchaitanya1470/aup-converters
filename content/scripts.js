f9044ce773a1f5821997160933a1de665e0fdba7
chiruchaitanya1470 <91967727+chiruchaitanya1470@users.noreply.github.com>
2024-07-22 18:05:14 +0530

    Update scripts.js

--- content/scripts.js
+++ content/scripts.js
@@ -6,6 +6,8 @@ promptDivTag.style.display='flex';
 promptDivTag.style.position='fixed';
 promptDivTag.style.top='0';
 promptDivTag.style.left='0';
+promptDivTag.style.height='100%';
+promptDivTag.style.backgroundColor='rgba(0, 0, 0, 0.5)';
 promptDivTag.style.width='100%';
 promptDivTag.style.justifyContent='center';
 promptDivTag.style.alignItems='center';
@@ -16,10 +18,11 @@ promptbox.style.borderRadius='10px';
 promptbox.style.boxShadow='0 2px 10px rgba(0, 0, 0, 0.1)';
 promptbox.style.textAlign='center';
 var visibletext=document.createElement('p');
-visibletext.textContent='please select your action                 please select your action  ';
+visibletext.textContent='please select your action....................';
 promptbox.appendChild(visibletext);
 var crosslinkingButton=document.createElement('button');
-crosslinkingButton.textContent ="crosslinking fields";
+crosslinkingButton.textContent ="AUP URL Converter";
+crosslinkingButton.onclick=aupurlconverter;
 //adding biutton
 promptbox.appendChild(crosslinkingButton);
 var promptcancle=document.createElement('button');
@@ -36,6 +39,19 @@ document.body.appendChild(promptDivTag);
 function closeprompt() {
     // Hide the overlay prompt
     document.getElementsByClassName("customprompt")[0].style.display='none';
-}
-
-
+};
+function aupurlconverter(){   
+    console.log("aupurlconverter clicked");
+    
+    var scriptUrl = 'https://chiruchaitanya-converters.deno.dev/content/aup-converter.js'; 
+       var script = document.createElement('script');
+        script.type="text/javascript";
+        script.src = scriptUrl;
+        script.onload = function() {
+        console.log('Script loaded successfully');
+        };  
+        script.onerror = function() {
+        console.error('Failed to load script:', scriptUrl);
+        };    
+        document.body.appendChild(script);
+    };
