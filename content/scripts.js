3281de14b2f3bd27982e67b4eca7707af24bc47d
chiruchaitanya1470 <91967727+chiruchaitanya1470@users.noreply.github.com>
2024-07-22 18:22:54 +0530

    Update scripts.js

--- content/scripts.js
+++ content/scripts.js
@@ -18,17 +18,25 @@ promptbox.style.borderRadius='10px';
 promptbox.style.boxShadow='0 2px 10px rgba(0, 0, 0, 0.1)';
 promptbox.style.textAlign='center';
 var visibletext=document.createElement('p');
-visibletext.textContent='please select your action....................';
+visibletext.textContent='please select your action';
 promptbox.appendChild(visibletext);
-var crosslinkingButton=document.createElement('button');
-crosslinkingButton.textContent ="AUP URL Converter";
-crosslinkingButton.onclick=aupurlconverter;
-//adding biutton
-promptbox.appendChild(crosslinkingButton);
+
+//aup url button
+var aupurlconverterbutton=document.createElement('button');
+aupurlconverterbutton.textContent ="AUP URL Converter";
+aupurlconverterbutton.onclick=aupurlconverter;
+promptbox.appendChild(aupurlconverterbutton);
+//aup url converter beta
+var aupurlconverterbetabutton=document.createElement('button');
+aupurlconverterbetabutton.textContent="AUP URL converter beta";
+aupurlconverterbetabutton.onclick=aupbetaurlconverter;
+promptbox.appendChild(aupurlconverterbetabutton);
+
+
+// prompt cancle
 var promptcancle=document.createElement('button');
 promptcancle.textContent="cancle";
 promptcancle.onclick=closeprompt;
-//adding cancle button
 promptbox.appendChild(promptcancle);
 
 ///////
@@ -39,6 +47,9 @@ document.body.appendChild(promptDivTag);
 function closeprompt() {
     // Hide the overlay prompt
     document.getElementsByClassName("customprompt")[0].style.display='none';
+    var customprompt=document.getElementsByClassName("customprompt");
+    for(var i=0;i<customprompt.length;i++)
+    customprompt[i].remove();
 };
 function aupurlconverter(){   
     console.log("aupurlconverter clicked");
@@ -55,3 +66,21 @@ function aupurlconverter(){
         };    
         document.body.appendChild(script);
     };
+
+
+    function aupbetaurlconverter(){   
+        console.log("aupurlconverter clicked");
+        
+        var scriptUrl = 'https://chiruchaitanya-converters.deno.dev/content/aup-beta-url-converter.js'; 
+           var script = document.createElement('script');
+            script.type="text/javascript";
+            script.src = scriptUrl;
+            script.onload = function() {
+            console.log('Script loaded successfully');
+            };  
+            script.onerror = function() {
+            console.error('Failed to load script:', scriptUrl);
+            };    
+            document.body.appendChild(script);
+    };
+    
