0b7644877d79596e20e8dfd6b985993963bbbf52
chiruchaitanya1470 <91967727+chiruchaitanya1470@users.noreply.github.com>
2024-07-22 14:23:04 +0530

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,12 @@
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { serveDir } from "https://deno.land/std@0.140.0/http/file_server.ts";
+
+serve((req) => {
+  const pathname = new URL(req.url).pathname;
+  if (pathname.startsWith("/static")) {
+    return serveDir(req, {
+      fsRoot: `${Deno.cwd()}`,
+    });
+  }
+  return new Response("hello world");
+});
