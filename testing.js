5944b92527b7dc02c49d3ac80d07da0a8118c21c
chiruchaitanya1470 <91967727+chiruchaitanya1470@users.noreply.github.com>
2024-07-22 15:30:07 +0530

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,17 +1,14 @@
-console.log("chiru chaitanya");
-console.log("chiru chaitanya");
-console.log("chiru chaitanya");
-console.log("chiru chaitanya");
-console.log("chiru chaitanya");
-console.log("chiru chaitanya");
-console.log("chiru chaitanya");
-console.log("chiru chaitanya");
-console.log("chiru chaitanya");
-console.log("chiru chaitanya");
-console.log("chiru chaitanya");
-console.log("chiru chaitanya");
-console.log("chiru chaitanya");
-console.log("chiru chaitanya");
-console.log("chiru chaitanya");
-console.log("chiru chaitanya");
-console.log("changes are done");
+javascript:(function(){   
+    
+var scriptUrl = 'https://cdn.jsdelivr.net/gh/chiruchaitanya1470/aup-converters/testing.js'; 
+   var script = document.createElement('script');
+    script.type="text/javascript";
+    script.src = scriptUrl;
+    script.onload = function() {
+    console.log('Script loaded successfully');
+    };  
+    script.onerror = function() {
+    console.error('Failed to load script:', scriptUrl);
+    };    
+    document.body.appendChild(script);
+})();
